faa940734cf4fa039c4b115c12ece70cf6787c44
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-02-10 00:42:29 +0300

    adding ans submit buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './components/TodoList';
+import { createTheme,ThemeProvider  } from '@mui/material/styles';
+const theme = createTheme({
+  typography:{
+    fontFamily:["Alexandria"]
+  }
+});
 
 function App() {
   return (
+    <ThemeProvider  theme={theme}>
     <div className="App" style={{display: "flex", direction:"rtl", justifyContent: "center" , alignItems :" center" ,background :"#607d8b" , height:"100vh"}}>
       <TodoList/>
     
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,6 +16,8 @@ import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
 import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
 import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
 import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import Grid from '@mui/material/Grid2';
+import TextField from '@mui/material/TextField';
 // Components 
 import Todo from "./Todo";
 export default function TodoList() {
@@ -24,6 +26,7 @@ export default function TodoList() {
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography variant="h2" 
+          style={{fontWeight:"bold"}}
           >
             مهامي
           </Typography>
@@ -51,10 +54,20 @@ export default function TodoList() {
       
     </ToggleButtonGroup>
         
-          {/* Filter Buttons  */}
+          {/* ---Filter Buttons  */}
           {/* All Todos  */}
           <Todo/>
-          {/* All Todos  */}
+          {/*--- All Todos  */}
+          {/* input and add button */}
+          <Grid container style={{marginTop:"20px"}} spacing={2}>  
+            <Grid size={8} display="flex" justifyContent="space-around" alignItems="center"  
+             > <TextField style={{width:"100%"}} id="outlined-basic" label="عنوان المهمة" variant="outlined" /></Grid>
+              <Grid size={4} display="flex" justifyContent="space-around" alignItems="center"  
+             >   <Button style={{width:"100%" ,height:"100%"}} variant="contained"> اضافة</Button></Grid>
+           
+            
+             </Grid>
+          {/*-- input and add button */}
           
          
         </CardContent>
