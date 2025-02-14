d825af8a8bab3b15b89c687d8b85f8e392441c5a
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-02-15 00:30:48 +0300

    deleted todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,11 +10,19 @@ import CheckIcon from '@mui/icons-material/Check';
 import IconButton from '@mui/material/IconButton';
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
-import { useContext } from "react";
+import { useContext ,useState } from "react";
 import { TodosContext } from "../context/todosContext";
+// Dialog 
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default function Todo({todo,handleCkeck}){
+  const [showDeleteDialog,setShowDeleteDialog]=useState(false)
   const {todos,setTodos  } = useContext(TodosContext);
+  // EVENTS HANDLERS 
   function handleCheckClick (){
     const updateTodo = todos.map((t) => {
       if(t.id=== todo.id)
@@ -23,7 +31,47 @@ export default function Todo({todo,handleCkeck}){
     });
     setTodos(updateTodo)
   }
-    return((<> <Card className="todoCard" sx={{ minWidth: 275  ,background:"#283593" ,color:"white" ,marginTop : 5}}>
+ function handleDeleteClick(){
+  setShowDeleteDialog(true)
+ }
+ function handleClose(){
+  setShowDeleteDialog(false)
+ }
+ function handleDeleteConfirm(){
+  const updatedTodos = todos.filter((t)=>{
+    return t.id != todo.id
+  })
+  setTodos(updatedTodos)
+ } 
+     // ----EVENTS HANDLERS 
+
+    return((<>
+    {/* Delete dialog  */}
+    <Dialog
+    style={{direction:"rtl"}}
+        open={showDeleteDialog}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+        هل أنت متأكد انك تريد حذف هذه المهمة؟       </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+           لا يمكن التراجع عن الحذف بعد  اتمام حذف المهمة 
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>اغلاق</Button>
+          <Button autoFocus onClick={handleDeleteConfirm}>
+           نعم . قم بالحذف 
+          </Button>
+        </DialogActions>
+      </Dialog>
+    {/* -----Delete dialog  */}
+    
+    
+     <Card className="todoCard" sx={{ minWidth: 275  ,background:"#283593" ,color:"white" ,marginTop : 5}}>
         <CardContent>
         <Grid container spacing={2}>
         <Grid size={8} >
@@ -51,9 +99,11 @@ export default function Todo({todo,handleCkeck}){
         <IconButton  className="iconButton" style={{color:"#1769aa", background:"white", border: "solid #1769aa 3px"}}>
         < ModeEditOutlineOutlinedIcon/>
       </IconButton>
-        <IconButton  className="iconButton" style={{color:"#b23c17", background:"white", border: "solid #b23c17 3px"}}>
+      {/* Delete dialog  */}
+        <IconButton onClick={handleDeleteClick} className="iconButton" style={{color:"#b23c17", background:"white", border: "solid #b23c17 3px"}}>
         < DeleteOutlineOutlinedIcon/>
       </IconButton>
+      {/* -----Delete dialog  */}
         </Grid>
             {/* Action Button  */}
       </Grid>
